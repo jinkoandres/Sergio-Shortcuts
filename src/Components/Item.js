688386ad89f82b60b8b85c4f757fae8a163a32dc
Andres' Work <andres@reactable.com>
2017-11-30 14:59:00 +0100

    This is what I wanted to show sergio

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -7,28 +7,35 @@ import {
 
 export default class Item extends React.Component {
     render() {
+        const { listRowStyle, titleStyle, releaseYearStyle} = styles;
+        const { title , releaseYear } = this.props;
         return (
-            <View style={styles.elemento}> 
-                <Text >{this.props.title}</Text>
-                <Text style={styles.releaseYear}>{this.props.releaseYear}</Text>
+            
+            <View style={listRowStyle}> 
+                <Text style = {titleStyle}>{title}</Text>
+                <Text style = {releaseYearStyle}>Year : {releaseYear}</Text>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    elemento:{
+    listRowStyle: {
         height:48,
         borderWidth:1,
         borderColor:'blue',
         paddingTop:10,
         paddingLeft:5,
         flexDirection:'row'
-
       },
-    releaseYear:{
-        
-        alignSelf:'flex-end'
+    titleStyle: {
+        flex: 2,
+        borderColor:'green',
+        borderWidth:1
+    },
+    releaseYearStyle: {
+        borderColor:'red',
+        borderWidth:1
     }
 })
 
